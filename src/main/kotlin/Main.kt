a9a65d24676ffd7a51d9c6b66e6837a01ca88dc4
CodeNinja-tech <elo48858@gmail.com>
2022-10-29 23:17:02 +0100

    Used Char and Boolean Data Type in Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+  val myCharValue: Char = 'a'  // Note that chars use single quotes
+  val myBoolean: Boolean = true
+  val falseBoolean: Boolean = false
+
+  println("This is a char: $myCharValue")
+  println("Truthiness of myBoolean variable: $myBoolean")
+  println("Is this falseBoolean true?: $falseBoolean")
+}
